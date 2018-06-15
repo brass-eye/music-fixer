caa92a66bc13dd7af88b012f0403937f43afd57a
Aiden Scott <aidenscott12@googlemail.com>
2018-06-15 10:09:13 +0100

    wip

--- find-compilations/walk.py
+++ find-compilations/walk.py
@@ -4,7 +4,8 @@ import mutagen
 path_str = '/run/media/aiden/ff338d33-962c-47d0-87b9-64849ab5c715/Music2/Music'
 home_music = '~/Music'
 
-rootDir = os.path.expanduser(home_music)
+rootDir = os.path.expanduser(path_str)
+various_failures = []
 for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
 
     directory_artists = {}
@@ -19,17 +20,18 @@ for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
                     #lower case it and warn if theres some case mismatch 
                     
             except AttributeError as identifier:
-                #print("\t\tfound non music file: " + path )
-                pass
+                various_failures.append("\t\tfound non music file: " + path)
             except KeyError as error:
-                #print("\t\tfound non music file: " + path )
-    #            print ('error on path {}'.format(path))
-                pass
+               various_failures.append('error on path {}'.format(path))
             except TypeError as error:
-                #print("\t\tfound non music file: " + path )
-                # print ('header erorr on path {}'.format(path))
-                pass
+                various_failures.append('header erorr on path {}'.format(path))
+            except mutagen.mp3.HeaderNotFoundError as error:
+                various_failures.append('header not found erorr on path {}'.format(path))
+            except mutagen.flac.error as error:
+                various_failures.append('header not found erorr on path {}'.format(path))
 
             # if the unique artists > 1 then set all the tracks to have TPE = ?
     if len(directory_artists) > 1:
         print("{} is probably a comp".format(dirName))
+for failure in various_failures:
+    print()
